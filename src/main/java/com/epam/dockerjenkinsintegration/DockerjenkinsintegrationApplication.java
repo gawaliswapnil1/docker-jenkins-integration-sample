15d1dcd44d97513756fad6ef63d653be6ade989e
Swapnil Gawali <swapnil_gawali@epam.com>
2022-06-23 10:50:46 +0530

    first commit

---
+++ src/main/java/com/epam/dockerjenkinsintegration/DockerjenkinsintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.epam.dockerjenkinsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerjenkinsintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerjenkinsintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/dockerjenkinsintegration/DockerjenkinsintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.dockerjenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinsintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
